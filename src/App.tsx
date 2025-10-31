7ad20d6c67d2aff80f721b86152f7546e4db13ea
IamShafeeq <Shafeeq.August@gmail.com>
2025-10-31 09:39:26 +0000

    4th Commit

--- src/App.tsx
+++ src/App.tsx
@@ -31,14 +31,14 @@
 //import App1 from   "./components/12-Routing/6/3/App"
 //import App1 from   "./components/12-Routing/6-Loaders/4/App"
 //import App1 from   "./components/12-Routing/7-Actions/1/App"
-//import App1 from   "./components/Tasks/1/App"
-//import App1 from   "./components/Tasks/2/App"
+//import App1 from   "./components/Tasks/2-Protected-Routes/App"
+ import App1 from   "./components/Tasks/2-Protected-Routes/App"
 //import App1 from   "./components/13-Redux/1/App"
 //import App1 from   "./components/14-TanStakQuery/1/App"
 //import App1 from   "./components/14-TanStakQuery/2/App"
 //import App1 from   "./components/14-TanStakQuery/3/App"
 //import App1 from   "./components/14-TanStakQuery/4/App"
-import App1 from   "./components/14-TanStakQuery/5/App"
+//import App1 from   "./components/14-TanStakQuery/5/App"
 
 function App() {
   return (
